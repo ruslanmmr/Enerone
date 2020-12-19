732450b9b2304f24d366e38ebf043c3c41a9f037
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2020-12-19 13:25:52 +0200

    eddited

--- build/scripts/global.js
+++ build/scripts/global.js
@@ -563,4 +563,58 @@ function prepareContentTablesToDisplay() {
     });
   }
 }
+
+$(document).ready(function (event) {
+  popup.init(); //popup.open($('#info'));
+});
+window.popup = {
+  init: function init() {
+    this.$trigger = $('[data-popup-open]');
+    this.$close = $('[data-popup-close]');
+    this.$trigger.on('click', function (event) {
+      event.preventDefault();
+      var $popup = $($(this).attr('href'));
+
+      if ($popup.length) {
+        popup.open($popup);
+      }
+    });
+    $(document).on('click', function (event) {
+      var $target = $(event.target);
+
+      if ($target.closest(popup.$close).length || $target.closest('.popup').length && $target.closest('.popup-block__container').length == 0) {
+        event.preventDefault();
+        var $popup = $target.closest('.popup').length ? $target.closest('.popup') : $('.city-question-popup');
+        popup.close($popup);
+      }
+    });
+  },
+  open: function open($popup) {
+    var _this = this;
+
+    var event = function event() {
+      _this.active = $popup;
+      scrollLock.disablePageScroll();
+      $popup.addClass('active');
+    };
+
+    if (this.active) {
+      popup.close(this.active, function () {
+        event();
+      });
+    } else {
+      event();
+    }
+  },
+  close: function close($popup, callback) {
+    var _this2 = this;
+
+    scrollLock.enablePageScroll();
+    $popup.removeClass('active');
+    setTimeout(function () {
+      _this2.active = undefined;
+      typeof callback === 'function' && callback();
+    }, 250);
+  }
+};
 //# sourceMappingURL=maps/global.js.map
