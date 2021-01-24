569ca0bcab8dccdc164d7f3a07705a1cb66b4d33
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-01-24 19:03:01 +0200

    eddited

--- build/scripts/lk.js
+++ build/scripts/lk.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 $(document).ready(function () {
   toggle();
   calendar();
@@ -214,13 +220,40 @@ function formElements() {
 
   $(document).on('change', '.form-upload__input', function (event) {
     var $this = $(this),
-        $label = $this.prev(),
+        $parent = $this.parent(),
+        $names = $parent.find('.form-upload__names'),
+        $label = $parent.find('.form-upload__label'),
         val = $this.val();
 
     if (val) {
-      var count = $this[0].files.length;
+      var count = $this[0].files.length,
+          names = [],
+          i = 0;
+
+      var _iterator = _createForOfIteratorHelper($this[0].files),
+          _step;
+
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var file = _step.value;
+          names[i] = file.name;
+          i++;
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+
+      if ($names.length) {
+        $names.text(names.join(', '));
+      } else {
+        $label.after("<span class='form-upload__names'>".concat(names.join(', '), "</span>"));
+      }
+
       $label.text("\u0417\u0430\u0433\u0440\u0443\u0436\u0443\u0435\u043D\u043E \u0444\u0430\u0439\u043B\u043E\u0432: ".concat(count));
     } else {
+      if ($names.length) $names.remove();
       $label.text('Загрузить файлы');
     }
   });
